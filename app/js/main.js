00dfa450fc0801fc280f60a04a8e84f64b864a05
mriliya <mr.litvishchenko@gmail.com>
2020-05-27 19:15:58 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
 $(function(){
 
+	$(".owl-carousel").owlCarousel({
 
+		items: 1,
+		dots: true,
+		lazyLoad: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		smartSpeed: 1000,
+		loop: true
+
+	});
 
   
 });
